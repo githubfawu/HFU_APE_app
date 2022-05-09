da4bbbdabd91176a31e7715c3b491efba6831de8
githubfawu <89583483+githubfawu@users.noreply.github.com>
2022-05-09 20:03:18 +0200

    .

---
+++ TestApp1/TestApp1/Models/ItemNew.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TestApp1.Models
+{
+    public class ItemNew
+    {
+        public string IdNew { get; set; }
+        public string TextNew { get; set; }
+        public string DescriptionNew { get; set; }
+    }
+}
